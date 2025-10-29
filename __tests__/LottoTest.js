96bd6ab077e66d98a7d749ac095c82aa6921aec6
zzzryt <jinjinstar3@gmail.com>
2025-10-29 21:59:36 +0900

    test(Lotto): 로또 클래스에 대한 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,25 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('로또 번호가 1 ~ 45 사이의 수가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 50]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('보너스 번호가 로또 번호와 중복된 숫자면 예외가 발생한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.bounce(5)).toThrow('[ERROR]');
+  });
+
+  test('보너스 번호가 1 ~ 45 사이 수가 아니면 예외가 발생한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.bounce(50)).toThrow('[ERROR]');
+  });
 });
